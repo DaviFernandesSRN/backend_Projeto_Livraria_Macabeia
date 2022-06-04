5e13abe783159755ef44f696a63a1f5e72a2b36e
Davi Fernandes <davifernandessrn@gmail.con>
2022-06-03 21:46:03 -0300

    add pastas

---
+++ Controllers/app.js
@@ -0,0 +1,32 @@
+import express from "express"; 
+////conexão com o banco de dados feita pelo mongoose
+import db from "../config/dbconnect.js"
+//importando o arquivo de rotas 
+import routes from "../routes/index.js"
+import cors from 'cors';
+
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow","*");
+    res.header("Access-Control-Allow-Methods","GET, PUT, POST, DELETE");
+    app.use(cors())
+    next();
+});
+
+
+
+
+db.on("error", console.log.bind(console, 'Erro de conexão com banco de dados'))
+db.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso")
+})
+
+//instancia do express
+const app = express();
+//body-parser usando o express
+app.use(express.json())
+//usando a rota 
+routes(app);
+
+
+
+export default app;
