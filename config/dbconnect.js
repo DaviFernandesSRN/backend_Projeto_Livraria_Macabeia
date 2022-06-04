1bb184e02d8168e2d3cbcbfa874cccd684df4e11
Davi Fernandes <davifernandessrn@gmail.con>
2022-06-03 21:47:10 -0300

    add senha

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,7 +1,7 @@
 //importando o mongoose para uso do mongo DB
 import mongoose from "mongoose"
 //Chamando o meu banco de dados no mongo DB
-mongoose.connect("mongodb+srv://admmacabeia:<lmacabeia>@cluster0.5ay0tuk.mongodb.net/livrariamacabeia");
+mongoose.connect("mongodb+srv://admmacabeia:lmacabeia@cluster0.5ay0tuk.mongodb.net/livrariamacabeia");
 //Criando uma váriavel da conexão
 let db = mongoose.connection;
 //exportanto a variavel criada da conexão
