67641557be1c55b6e7a86430c891c30af3a66089
Davi Fernandes <davifernandessrn@gmail.con>
2022-06-06 22:12:05 -0300

    adicionando buscar por titulo

--- src/models/Livros.js
+++ src/models/Livros.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema(
     {
          id: {type: String},
-        "titulo": {type: String, required: true},
+        titulo : {type: String, required: true},
         "autor": {type: String, required: true},
         "sinopse": {type: String, required: true},
         "categoria": {type: String, required: true},
